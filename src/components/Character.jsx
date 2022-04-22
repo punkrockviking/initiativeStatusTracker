77ee36cb97179b412e84d724d753e164b49ec52d
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2022-04-22 00:30:15 +0000

    add name, initiative, and team color to Character component

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -6,7 +6,32 @@ function Character() {
   return (
     <div>
       <div>
-        Single Character block here
+        <label>
+          Name
+          <input 
+            value='Jari Bloodborne' 
+            type='string'
+            name='name'
+          />
+        </label>
+        <label>
+          Initiative
+          <input 
+            value='10' 
+            type="number"
+            name="level"
+            min="1"
+            max="30"
+          />
+        </label>
+        <label>
+          Team
+          <input 
+            value='green' 
+            type="string"
+            name='team'
+          />
+        </label>
       </div>
     </div>
   )
