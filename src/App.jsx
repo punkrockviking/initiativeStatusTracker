6c2fa83a1ed448735ecf01a6800163e777c6eb5d
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2022-04-23 22:49:25 +0000

    convert Character to class based component in order to follow tutorial on creating single handle method for all form entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import Character from './components/Character'
+// import Character from './components/Character'
+import Character from './components/CharacterClass'
+
 import './App.css';
 
 function App() {
---
+++ src/components/CharacterClass.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+class Character extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initScore: 1,
+      charName: 'name',
+      teamColor: 'green',
+      charNotes: [],
+      concentrate: false,
+    }
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.handleOnSubmit = this.handleOnSubmit.bind(this)
+  }
+
+  
+  
+  
+  handleOnChange(event) {
+    event.preventDefault()
+    const { name, value } = event.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault()
+  }
+
+  
+  render() {
+    return (
+      <div>
+        <div>
+          <form
+            onChange={this.handleOnChange}
+            onSubmit={this.handleOnSubmit}
+          >
+            <label>
+              Name
+              <input 
+                value={this.state.charName} 
+                type="string"
+                name="charName"
+              />
+            </label>
+              <div>
+                Initiative
+                <input 
+                  value={this.state.initScore}
+                  type="number"
+                  name="initScore"
+                  min="1"
+                  max="30"
+                />
+              </div>
+            <label>
+              Team
+              <input 
+                value={this.state.teamColor} 
+                type="string"
+                name="teamColor"
+              />
+            </label>
+          </form>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Character
