4bf834568b9094bb157878f1db417ec4e740aaa9
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2022-06-22 01:31:41 +0000

    (feat) added team color Dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Dropdown = () => {
-  return <h1>Dropdown</h1>
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <option key={option.value} >
+        {option.label}
+      </option>
+    )
+  })
+  
+  return (
+    <div>
+      <form>
+        <label>Team Color: </label>
+          <select>
+            {renderedOptions}
+          </select>
+      </form>
+    </div>
+  )
 }
 
 export default Dropdown;
